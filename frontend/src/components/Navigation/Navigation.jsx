8b4bd039f203bd2a94a9ca4d1e816252eb22a48e
dhananjaykadam002 <kadamdhanu100@gmail.com>
2025-02-06 15:07:29 +0530

    asch

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import './Navigation.css';
 import { Link } from "react-router-dom";
+import { BsArrowThroughHeartFill } from "react-icons/bs";
+
 
 const Navigation = () => {
   return (
     <div className="navigation_section">
       <Link to="/" className="logo">
-        HMS
+        <div className="hospital_name"><span><BsArrowThroughHeartFill /></span> Hospital Name</div>
       </Link>
       <div className="nav_links">
         <Link to="/registration" className="reg">
